62729b9870c5f2aba5150b631bc9150e4148ce08
Kurt Barbarich <kbarbz1995@gmail.com>
2023-11-16 21:32:48 +0800

    Add collision

--- src/main.py
+++ src/main.py
@@ -21,16 +21,16 @@ delay_counter = move_delay
 font = pygame.font.Font(None, 36)
 
 while running:
-    live_t = tetrominos[current_tetromino]
+    live_t = tetrominos[0]
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and live_t.can_go_left():
+            if event.key == pygame.K_LEFT and live_t.can_go_left() and not tetris_game.check_for_collision(live_t.shape, live_t.x - 1, live_t.y):
                 live_t.move(-move_speed, 0)
-            elif event.key == pygame.K_RIGHT and live_t.can_go_right(board):
+            elif event.key == pygame.K_RIGHT and live_t.can_go_right(board) and not tetris_game.check_for_collision(live_t.shape, live_t.x + 1, live_t.y):
                 live_t.move(move_speed, 0)
             elif event.key == pygame.K_UP:  # Rotate the sprite when 'R' is pressed
                 live_t.rotate_clockwise(board)
@@ -42,9 +42,9 @@ while running:
     delay_counter -= move_speed
     if delay_counter <= move_speed:
         live_t.move(0, 1)
-        if live_t.is_at_bottom(board):
-            tetrominos.append(tetris_game.generate_tetromino()) # Wrap to the top
-            current_tetromino += 1
+        if live_t.is_at_bottom(board) or tetris_game.check_for_collision(live_t.shape, live_t.x, live_t.y + 1):
+            tetris_game.add_to_landed(live_t)
+            tetrominos[0] = tetris_game.generate_tetromino()
         delay_counter = move_delay
 
     # DEBUGGER
@@ -56,6 +56,7 @@ while running:
     for t in tetrominos:
         t.draw(board.screen)
     board.draw_grid()
+    tetris_game.draw_game_grid()
     board.screen.blit(text, text_rect)
     board.update()
 
--- src/tetris.py
+++ src/tetris.py
@@ -1,11 +1,35 @@
 import block
 import random
-
+import pygame
+from utils import Dimensions
 
 class TetrisGame():
     def __init__(self, board):
         self.board = board
+        self.game_grid = self.generate_grid()
 
     def generate_tetromino(self):
         block_class = random.choice(block.Tetromino.__subclasses__())
         return block_class(self.board)
+
+    def generate_grid(self):
+        return [[0 for _ in range(self.board.width)] for _ in range(self.board.height)]
+
+    def add_to_landed(self, tetromino):
+        for row in range(len(tetromino.shape)):
+            for col in range(len(tetromino.shape[row])):
+                if tetromino.shape[row][col]:
+                    self.game_grid[tetromino.y + row][tetromino.x + col] = tetromino.color
+
+    def draw_game_grid(self):
+        for row in range(len(self.game_grid)):
+            for col in range(len(self.game_grid[row])):
+                if self.game_grid[row][col]:
+                    pygame.draw.rect(self.board.screen, self.game_grid[row][col], (col * Dimensions.CELL_SIZE.value, row * Dimensions.CELL_SIZE.value, Dimensions.CELL_SIZE.value, Dimensions.CELL_SIZE.value))
+
+    def check_for_collision(self, shape, x, y):
+        for row in range(len(shape)):
+            for col in range(len(shape[row])):
+                if shape[row][col]:
+                    if self.game_grid[y + row][x + col]:
+                        return True
